6482af2a4f531b9a684693624b107929c887ea46
Enes <enesay233@gmail.com>
2024-09-14 21:23:32 +0300

    Update stock dao, gitignore

--- app/src/main/java/com/example/financialliteracy/retrofit/StockDao.kt
+++ app/src/main/java/com/example/financialliteracy/retrofit/StockDao.kt
@@ -1,4 +1,15 @@
 package com.example.financialliteracy.retrofit
 
+import com.example.financialliteracy.model.StockResponse
+import com.example.financialliteracy.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface StockDao {
+
+    @GET("https://api.stockdata.org/v1/data/quote HTTP/1.1")
+    fun getStocks(
+        @Query("api_token") apiToken: String = Constants.API_KEY,
+        @Query("symbols") symbols: String = "TSLA"
+    ) : StockResponse
 }
