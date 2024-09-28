a8965297ad262a26f076e1daadf55c74dc6c45d3
Enes <enesay233@gmail.com>
2024-09-29 01:31:08 +0300

    Update sign-up ui

--- app/src/main/java/com/example/financialliteracy/ui/presentation/Register/RegisterScreen.kt
+++ app/src/main/java/com/example/financialliteracy/ui/presentation/Register/RegisterScreen.kt
@@ -1,17 +1,28 @@
 package com.example.financialliteracy.ui.presentation.Register
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -20,65 +31,69 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.financialliteracy.ui.theme.primary_color
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Register(modifier: Modifier = Modifier, navController: NavController) {
-    val email = remember { mutableStateOf("") }
-    val password = remember { mutableStateOf("") }
-    val name = remember { mutableStateOf("") }
 
-    val registerViewmodel:RegisterViewmodel = hiltViewModel()
+    val registerViewmodel: RegisterViewmodel = hiltViewModel()
     val authState by registerViewmodel.authState
 
-    Scaffold (modifier = Modifier.fillMaxSize()) { paddingValues ->
-
-        val name = remember { mutableStateOf("") }
+    Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
         val email = remember { mutableStateOf("") }
         val password = remember { mutableStateOf("") }
-        val nameError = remember { mutableStateOf<String?>(null) }
         val emailError = remember { mutableStateOf<String?>(null) }
         val passwordError = remember { mutableStateOf<String?>(null) }
 
-        Column(
-            Modifier
+        Box(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(paddingValues)
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+                .padding(24.dp),
+            contentAlignment = Alignment.Center
         ) {
             Column(
-                modifier = Modifier.size(300.dp),
-                verticalArrangement = Arrangement.SpaceEvenly,
+                verticalArrangement = Arrangement.spacedBy(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Name Field
-                TextField(
-                    value = name.value,
-                    onValueChange = {
-                        name.value = it
-                        nameError.value = null // Değer değiştirildiğinde hatayı sıfırla
-                    },
-                    label = { Text("Name") },
-                    isError = nameError.value != null // Hata durumu
+                Text(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(vertical = 20.dp),
+                    text = "Sign up",
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Medium,
+                    color = primary_color
                 )
-                if (nameError.value != null) {
-                    Text(text = nameError.value!!, color = Color.Red, fontSize = 12.sp)
-                }
 
                 // Email Field
                 TextField(
                     value = email.value,
                     onValueChange = {
                         email.value = it
-                        emailError.value = null // Değer değiştirildiğinde hatayı sıfırla
+                        emailError.value = null
                     },
                     label = { Text("Email") },
-                    isError = emailError.value != null // Hata durumu
+                    isError = emailError.value != null,
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            BorderStroke(2.dp, Color.Gray),
+                            shape = MaterialTheme.shapes.small
+                        )
                 )
                 if (emailError.value != null) {
                     Text(text = emailError.value!!, color = Color.Red, fontSize = 12.sp)
@@ -89,10 +104,22 @@ fun Register(modifier: Modifier = Modifier, navController: NavController) {
                     value = password.value,
                     onValueChange = {
                         password.value = it
-                        passwordError.value = null // Değer değiştirildiğinde hatayı sıfırla
+                        passwordError.value = null
                     },
                     label = { Text("Password") },
-                    isError = passwordError.value != null // Hata durumu
+                    isError = passwordError.value != null,
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            BorderStroke(2.dp, Color.Gray),
+                            shape = MaterialTheme.shapes.small
+                        )
+
                 )
                 if (passwordError.value != null) {
                     Text(text = passwordError.value!!, color = Color.Red, fontSize = 12.sp)
@@ -100,42 +127,62 @@ fun Register(modifier: Modifier = Modifier, navController: NavController) {
 
                 // Auth State Handling
                 when (authState) {
-                    is AuthState.Idle -> Button(onClick = {
-                        // Boş alan kontrolü yap
-                        if (name.value.isBlank()) {
-                            nameError.value = "Name cannot be empty"
+                    is AuthState.Idle ->
+                        Button(
+                            onClick = {
+                                if (email.value.isBlank()) {
+                                    emailError.value = "Email cannot be empty"
+                                }
+                                if (password.value.isBlank()) {
+                                    passwordError.value = "Password cannot be empty"
+                                }
+                                if (emailError.value == null && passwordError.value == null) {
+                                    registerViewmodel.signUp(email.value, password.value)
+                                }
+                            },
+                            modifier = Modifier.width(300.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = primary_color
+                            )
+                        ) {
+                            Text("Sign up", color = Color.White, fontSize = 17.sp)
                         }
-                        if (email.value.isBlank()) {
-                            emailError.value = "Email cannot be empty"
-                        }
-                        if (password.value.isBlank()) {
-                            passwordError.value = "Password cannot be empty"
-                        }
-
-                        // Eğer hata yoksa kaydı başlat
-                        if (nameError.value == null && emailError.value == null && passwordError.value == null) {
-                            registerViewmodel.signUp(email.value, password.value)
-                        }
-                    }) {
-                        Text("Register")
-                    }
 
                     is AuthState.Loading -> CircularProgressIndicator()
 
                     is AuthState.Authenticated -> {
                         Text(text = "Registration is Successful!")
-                        navController.navigate("home")
+                        navController.navigate("foodList")
                     }
 
                     is AuthState.Error -> {
                         Text(text = (authState as AuthState.Error).message, color = Color.Red)
-                        Button(onClick = {
-                            registerViewmodel.signUp(email.value, password.value)
-                        }) {
-                            Text("Register")
+                        Button(
+                            onClick = { registerViewmodel.signUp(email.value, password.value) },
+                            modifier = Modifier.width(300.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.error
+                            )
+                        ) {
+                            Text("Retry", color = Color.White, fontSize = 17.sp)
                         }
+
                     }
                 }
+                Button(
+                    onClick = {
+                        navController.navigate("login"){
+                            popUpTo("register")
+                        }
+                    },
+                    modifier = Modifier.width(300.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = primary_color
+                    )
+                ) {
+                    Text("Cancel", color = Color.White, fontSize = 17.sp)
+                }
+
             }
         }
 
