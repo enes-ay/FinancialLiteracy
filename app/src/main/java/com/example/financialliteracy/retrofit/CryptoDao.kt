5c9cbb71825f626d8be5756de318b3f2a1fa8e93
Enes <enesay233@gmail.com>
2024-10-09 23:50:13 +0300

    Add crypto dao

---
+++ app/src/main/java/com/example/financialliteracy/retrofit/CryptoDao.kt
@@ -0,0 +1,15 @@
+package com.example.financialliteracy.retrofit
+
+import com.example.financialliteracy.model.LatestResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface CryptoDao {
+    @GET("/v1/cryptocurrency/listings/latest")
+    suspend fun getLatestCryptos(
+        @Query("start") start: Int = 1,
+        @Query("limit") limit: Int = 10,
+        @Query("convert") convert: String = "USD"
+    ): LatestResponse
+}
