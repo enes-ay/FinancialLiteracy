05e20a557df22999dc0b7e3cf4b615ed479cadb2
Enes <enesay233@gmail.com>
2024-10-10 12:26:15 +0300

    Add crypto module

---
+++ app/src/main/java/com/example/financialliteracy/di/CryptoModule.kt
@@ -0,0 +1,48 @@
+package com.example.financialliteracy.di
+
+import com.example.financialliteracy.retrofit.CryptoDao
+import com.example.financialliteracy.utils.Constants.API_KEY_CRYPTO
+import com.example.financialliteracy.utils.Constants.BASE_URL_CRYPTO
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CryptoModule {
+
+    @Provides
+    @Singleton
+    fun provideCryptoRepository(remoteDataSource: CryptoRemoteDataSource): CryptoRepository {
+        return CryptoRepositoryImpl(remoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCryptoRemoteDataSource(apiService: CryptoDao): CryptoRemoteDataSource {
+        return CryptoRemoteDataSource(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(): CryptoDao {
+        val apiKey = API_KEY_CRYPTO
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(apiKey))
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL_CRYPTO)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CryptoDao::class.java)
+    }
+
+
+}
