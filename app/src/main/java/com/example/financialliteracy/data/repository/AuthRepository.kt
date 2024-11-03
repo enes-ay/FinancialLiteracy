de636f4714698bda5b7da9edd834aa1f36e57967
Enes Ay <enesay233@gmail.com>
2024-11-03 22:53:25 +0300

    Update auth repository for sign up initial balance

--- app/src/main/java/com/example/financialliteracy/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/financialliteracy/data/repository/AuthRepository.kt
@@ -4,20 +4,31 @@ import android.util.Log
 import com.example.financialliteracy.common.Resource
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.rpc.context.AttributeContext.Auth
 import kotlinx.coroutines.tasks.await
 import java.util.concurrent.Flow
 import javax.inject.Inject
 
-class AuthRepository @Inject constructor(private val firebaseAuth: FirebaseAuth, private val walletRepository: WalletRepository){
+class AuthRepository @Inject constructor(private val firebaseAuth: FirebaseAuth,
+                                         private val walletRepository: WalletRepository,
+    private val firestore: FirebaseFirestore){
 
     suspend fun signUp(email: String, password: String): Resource<String> {
         return try {
             val result = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
             val userId = result.user?.uid.orEmpty()
-
+            val userDoc = firestore.collection("Users").document(userId)
+            userDoc.set(
+                mapOf(
+                    "Balance" to 10000.0,
+                    "Assets" to emptyList<Map<String, Any>>(),
+                    "Transactions" to emptyList<Map<String, Any>>()
+                )
+            ).await()
             if (userId.isNotEmpty()) {
                 // Yeni kullanıcıya başlangıç bakiyesi atanır
+                Log.e("initial","balance verildi")
                 walletRepository.initializeUserData(userId, 10000.0)
             }
 
