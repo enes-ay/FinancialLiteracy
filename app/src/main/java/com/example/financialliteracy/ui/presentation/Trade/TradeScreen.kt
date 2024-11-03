7777a84a9b7e7b638ee942d8cd33cd08a8664353
Enes Ay <enesay233@gmail.com>
2024-11-03 19:29:35 +0300

    Update trade screen buy, sell onclick

--- app/src/main/java/com/example/financialliteracy/ui/presentation/Trade/TradeScreen.kt
+++ app/src/main/java/com/example/financialliteracy/ui/presentation/Trade/TradeScreen.kt
@@ -1,6 +1,6 @@
 package com.example.financialliteracy.ui.presentation.Trade
 
-import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,13 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,20 +24,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.financialliteracy.model.DataCrypto
-import com.example.financialliteracy.ui.presentation.Portfolio.Asset
+import com.example.financialliteracy.model.Trade.Asset
 import java.util.Locale
 
 @Composable
-fun TradeScreen(navController: NavHostController, asset: DataCrypto) {
+fun TradeScreen(navController: NavHostController, asset: Asset) {
     val tradeViewmodel : TradeViewmodel = hiltViewModel()
-    val formattedPrice = String.format(Locale.US,"%,.2f", asset.quote.USD.price)
+    val formattedPrice = String.format(Locale.US,"%,.2f", asset.price)
 
     Scaffold (Modifier.fillMaxSize()){ paddingValues->
 
@@ -72,24 +66,18 @@ fun TradeScreen(navController: NavHostController, asset: DataCrypto) {
 
             // Al ve Sat Butonları
             Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                Button(onClick = {  }, colors = ButtonDefaults.buttonColors(containerColor = Color.Green)) {
+                Button(onClick = {
+                    tradeViewmodel.buyAsset(asset, amount.toDouble())
+                    Toast.makeText(navController.context, "Alındı", Toast.LENGTH_SHORT).show()
+                }, colors = ButtonDefaults.buttonColors(containerColor = Color.Green)) {
                     Text("Buy", fontWeight = FontWeight.Bold)
                 }
-                Button(onClick = {  }, colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                Button(onClick = {
+                    tradeViewmodel.sellAsset(asset, amount.toDouble())
+                }, colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
                     Text("Sell", fontWeight = FontWeight.Bold)
                 }
             }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TradeScreenPreview() {
-    val mockAssets = listOf(
-        Asset(1, "Gold", 1500.0, symbol = "XAU"),
-        Asset(2, "Bitcoin", 20000.0),
-        Asset(3, "TSLA", 1000.0)
-    )
-    //TradeScreen(navController = rememberNavController(),asset = mockAssets.get(0))
 }
