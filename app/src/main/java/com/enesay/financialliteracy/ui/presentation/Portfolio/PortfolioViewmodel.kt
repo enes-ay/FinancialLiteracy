30896a675dcb8c7c071f8073749266360d24f3be
Enes Ay <enesay233@gmail.com>
2024-12-08 20:30:11 +0300

    Refactor realtime balance value in portfolio page

--- app/src/main/java/com/enesay/financialliteracy/ui/presentation/Portfolio/PortfolioViewmodel.kt
+++ app/src/main/java/com/enesay/financialliteracy/ui/presentation/Portfolio/PortfolioViewmodel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.enesay.financialliteracy.data.repository.WalletRepository
 import com.enesay.financialliteracy.model.Trade.Asset
+import com.enesay.financialliteracy.ui.presentation.AssetList.AssetListViewmodel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,7 +13,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PortfolioViewmodel @Inject constructor(val repository: WalletRepository) : ViewModel() {
+class PortfolioViewmodel @Inject constructor(
+    val repository: WalletRepository,
+    val assetListViewModel: AssetListViewmodel) : ViewModel() { /** Need to switch to use case **/
+
     private val _assets = MutableStateFlow<List<Asset>>(emptyList())
     val assets: StateFlow<List<Asset>> = _assets
 
@@ -25,19 +29,35 @@ class PortfolioViewmodel @Inject constructor(val repository: WalletRepository) :
     private var _userId = MutableStateFlow("")
     var userId: StateFlow<String> = _userId
 
+    // TODO: Use this function as use case
     fun getUserAssets(userId: String) {
         viewModelScope.launch {
             if (userId != null) {
                 val assets = repository.getUserAssets(userId)
                 _userAssets.value = assets
+                calculateBalance(userId)
             }
         }
     }
 
-     fun getBalance(userId: String) {
+    fun calculateBalance(userId: String) {
         viewModelScope.launch {
-            val balance = repository.getUserBalance(userId)
-            _balance.value = balance
+            val userAssets = repository.getUserAssets(userId) // Kullanıcı varlıkları
+
+            // Güncel fiyatlar
+            assetListViewModel.cryptoData.collect { cryptoList ->
+                val balance = userAssets.sumOf { asset ->
+                    val currentPrice =
+                        cryptoList.find { it.symbol == asset.symbol }?.price ?: 0.0
+                    asset.quantity * currentPrice
+                }
+                _balance.value = balance
+                _userAssets.value = userAssets.map { asset ->
+                    val currentPrice =
+                        cryptoList.find { it.symbol == asset.symbol }?.price ?: 0.0
+                    asset.copy(price = currentPrice)
+                }
+            }
         }
     }
 }
